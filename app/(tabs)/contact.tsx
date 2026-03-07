0b4bd4d90bd1e4c14125c67ed1b5712d2af5cc47
James <jamesleonardo230@gmail.com>
2026-03-06 20:35:03 -0500

    Mis cambios

--- app/(tabs)/contact.tsx
+++ app/(tabs)/contact.tsx
@@ -1,10 +1,14 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+
 
 export default function contact() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Contact screen</Text>
+      <Image style={styles.image}
+      source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI-PKOYFjaCBVh7PS9cWAQ5h4pEUTjAU7mMQ&s"}}/>
       <Link href="/" style={styles.buttonContact}>
         Devuelvase a home
       </Link>
@@ -13,6 +17,11 @@ export default function contact() {
 }
 
 const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height:  200,
+    
+  },
   container: {
     flex: 1,
     justifyContent: "center",
